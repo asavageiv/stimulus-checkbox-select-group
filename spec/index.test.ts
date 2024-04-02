6fe13167ef79db6179d1bd1eb12d48f2a976de5d
Alan Savage <3028205+asavageiv@users.noreply.github.com>
2024-04-02 13:20:47 -0700

    Add tests for the group-select behavior

--- spec/index.test.ts
+++ spec/index.test.ts
@@ -50,3 +50,64 @@ describe("#refresh", () => {
     expect(toggleCheckbox.indeterminate).toBe(true)
   })
 })
+
+describe("with multiple groups", () => {
+  beforeEach((): void => {
+    document.body.innerHTML = `
+      <form data-controller="checkbox-select-all">
+        <input id="checkbox-select-all-1" type="checkbox" data-checkbox-select-all-target="checkboxAll" data-checkbox-select-all-group="1" />
+        <input type="checkbox" data-checkbox-select-all-target="checkbox" data-checkbox-select-all-group="1" />
+        <input type="checkbox" data-checkbox-select-all-target="checkbox" data-checkbox-select-all-group="1" checked="checked" />
+        <input type="checkbox" data-checkbox-select-all-target="checkbox" data-checkbox-select-all-group="1" />
+        
+        <input id="checkbox-select-all-2" type="checkbox" data-checkbox-select-all-target="checkboxAll" data-checkbox-select-all-group="2" />
+        <input type="checkbox" data-checkbox-select-all-target="checkbox" data-checkbox-select-all-group="2" />
+        <input type="checkbox" data-checkbox-select-all-target="checkbox" data-checkbox-select-all-group="2" checked="checked" />
+        <input type="checkbox" data-checkbox-select-all-target="checkbox" data-checkbox-select-all-group="2" />
+      </form>
+    `
+  })
+
+  describe("#toggle", () => {
+    function checkedTargetsForGroup(groupNumber: number): NodeList {
+      return document.querySelectorAll(`[data-checkbox-select-all-target='checkbox'][data-checkbox-select-all-group='${groupNumber}']:checked`)
+    }
+
+    it("should select all checkboxes in the group", (): void => {
+      const toggleCheckbox1: HTMLInputElement = document.querySelector("#checkbox-select-all-1")
+      const toggleCheckbox2: HTMLInputElement = document.querySelector("#checkbox-select-all-2")
+
+      expect(checkedTargetsForGroup(1).length).toBe(1)
+      expect(checkedTargetsForGroup(2).length).toBe(1)
+
+      // Uncheck all
+      toggleCheckbox1.click()
+      expect(checkedTargetsForGroup(1).length).toBe(0)
+      expect(checkedTargetsForGroup(2).length).toBe(1)
+      toggleCheckbox2.click()
+      expect(checkedTargetsForGroup(1).length).toBe(0)
+      expect(checkedTargetsForGroup(2).length).toBe(0)
+
+      // Check all
+      toggleCheckbox1.click()
+      expect(checkedTargetsForGroup(1).length).toBe(3)
+      expect(checkedTargetsForGroup(2).length).toBe(0)
+      toggleCheckbox2.click()
+      expect(checkedTargetsForGroup(1).length).toBe(3)
+      expect(checkedTargetsForGroup(2).length).toBe(3)
+    })
+  })
+
+  // not meaningful for targets with a defined group number
+  describe("#refresh", () => {
+    it("change the checkboxAll state", (): void => {
+      const toggleCheckbox1: HTMLInputElement = document.querySelector("#checkbox-select-all-1")
+      const toggleCheckbox2: HTMLInputElement = document.querySelector("#checkbox-select-all-2")
+
+      expect(toggleCheckbox1.checked).toBe(true)
+      expect(toggleCheckbox1.indeterminate).toBe(true)
+      expect(toggleCheckbox2.checked).toBe(true)
+      expect(toggleCheckbox2.indeterminate).toBe(true)
+    })
+  })
+});
